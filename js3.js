a8077bfa2ee8971a0fa3baecfd0fe4cc3cfb0ba3
LoliaStarGirl <46056002+LoliaStarGirl@users.noreply.github.com>
2020-04-19 22:10:05 +0100

    Add files via upload

---
+++ js3.js
@@ -0,0 +1,25 @@
+function multipleFinder(num){
+    var result=[];
+    for(var i=1;i<=num;i++){
+        if((i%2===0)&&(i%3===0)&&(i%5===0))
+                result.push("yu-gi-oh");
+        else if((i%2===0)&&(i%3!=0)&&(i%5!=0))
+                result.push("yu");
+        else if((i%2!=0)&&(i%3===0)&&(i%5!=0))
+                result.push("gi");
+        else if((i%2!=0)&&(i%3!=0)&&(i%5===0))
+                result.push("oh");
+        else if((i%2===0)&&(i%3===0)&&(i%5!=0))
+                result.push("yu-gi");
+        else if((i%2===0)&&(i%3!=0)&&(i%5===0))
+                result.push("yu-oh");
+        else if((i%2!=0)&&(i%3===0)&&(i%5===0))
+                result.push("gi-oh");
+        else{
+                result.push(i);
+            }
+    }
+    return result;
+}
+multipleFinder(100);
+multipleFinder(30);
